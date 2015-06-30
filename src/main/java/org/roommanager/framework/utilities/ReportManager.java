1e8410bc3fce4e1974b05827a469a3d9012cd740
Pablo Sahonero <pablo.sahonero.2407@gmail.com>
2015-06-30 19:40:22 -0400

    Create Report Manager Class to edit the Html Report

---
+++ src/main/java/org/roommanager/framework/utilities/ReportManager.java
@@ -0,0 +1,35 @@
+package org.roommanager.framework.utilities;
+
+import org.testng.Reporter;
+
+public class ReportManager {
+	private static String REPORTNG_PROPERTY = "org.uncommons.reportng.escape-output";
+	
+	public static void appendImageHyperLink(String filePath){
+		System.setProperty(REPORTNG_PROPERTY , "false");
+		String failedTestImageLinkTag = "<a href=\"[filePath]"
+	            + "\"><img src=\"file:///[filePath]"
+	            + "\" alt=\"\"" + "height='100' width='100'/> <br />";
+		failedTestImageLinkTag = failedTestImageLinkTag
+				.replace("[filePath]", filePath);
+		
+		enableHtmlReportEdition();
+		Reporter.log(failedTestImageLinkTag); 
+	}
+	
+	public static void appendTestCaseErrorMessage(String testName, String message){
+
+		String errorMessageTag = "<a href=\"javascript:toggleElement('[testCaseName]', 'block')\" title=\"Click to expand/collapse\"><b>[testCaseName]</b></a>";
+		errorMessageTag = errorMessageTag + "<div class=\"stackTrace\" id=\"[testCaseName]\" style=\"display: none;\">[message]</div>";
+		errorMessageTag = errorMessageTag
+				.replace("[testCaseName]", testName)
+				.replace("[message]", message);
+		
+		enableHtmlReportEdition();
+		Reporter.log(errorMessageTag); 
+	}
+	
+	private static void enableHtmlReportEdition(){
+		System.setProperty(REPORTNG_PROPERTY , "false");
+	}
+}
