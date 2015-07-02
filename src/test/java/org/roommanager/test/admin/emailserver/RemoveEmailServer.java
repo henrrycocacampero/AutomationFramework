09f9331c1a2e99864049516cc601798f33814d67
MilencaVentura <Milenca.VenturaCanaviri@fundacion-jala.org>
2015-07-02 12:24:17 -0400

    refactor test case RegisterEmailserver and add test case remoeEmailServer

--- src/test/java/org/roommanager/test/admin/emailserver/RemoveEmailServer.java
+++ src/test/java/org/roommanager/test/admin/emailserver/RemoveEmailServer.java
@@ -1,34 +1,25 @@
 package org.roommanager.test.admin.emailserver;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.roommanager.framework.pages.admin.emailserver.EmailServerPage;
+import org.roommanager.framework.pages.admin.emailserver.RemoveEmailServerPage;
 import org.roommanager.framework.pages.admin.home.HomePage;
 import org.roommanager.framework.pages.admin.login.LoginPage;
+import org.roommanager.framework.utilities.common.TestBase;
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-public class RemoveEmailServer {
-	private static WebDriver driver = null;
+public class RemoveEmailServer extends TestBase {
 	
-	@BeforeSuite
-	  public void beforeSuite() {
-		driver= new FirefoxDriver();
-	  }
 	 @Test
 	  public void removeEmailServer(){
 		  String errorMessage = "The email server was not removed";
 		  driver.get("http://172.20.208.174:4044/admin/#/login");
-	
-		 
+		  LoginPage login = new LoginPage(driver);
+		  HomePage home = login.clickSignInButton();
+		  EmailServerPage emailServer = home.selectEmailServerLink();
+		  RemoveEmailServerPage removeEmailServer = emailServer.clickRemoveButton();
+		  emailServer = removeEmailServer.clickOnYesButton();
+		  Assert.assertTrue(emailServer.existsEmailServerRegistered(), errorMessage);
 	 }
-	  
-	  
-
-	  @AfterSuite
-	  public void afterSuite() {
-		  driver.quit();
-	  }
+	
 }
