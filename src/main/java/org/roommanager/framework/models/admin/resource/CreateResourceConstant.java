0cd3b90ee9e1da88735420374018477d036402e2
jimmy29011993 <jimmy2901993@gmail.com>
2015-07-07 18:32:03 -0400

    Documentation example

--- src/main/java/org/roommanager/framework/models/admin/resource/CreateResourceConstant.java
+++ src/main/java/org/roommanager/framework/models/admin/resource/CreateResourceConstant.java
@@ -1,12 +1,23 @@
 package org.roommanager.framework.models.admin.resource;
-
+/**
+ * The CreateResourceConstant class provides the locators from 
+ * Create Resource Tab.
+ * 
+ * @author Qadev02
+ * 
+ */
 public class CreateResourceConstant {
-	/**Locator Type = xpath*/
+        
+	/** RESOURCE_NAME_FIELD locator to Resource Name text field*/
     public static final String RESOURCE_NAME_FIELD="(//input[@type='text'])[3]";
-    /**Locator Type = xpath*/
+    
+    /** RESOURCE_DISPLAY_NAME_FIELD locator to Resource Display Name text field*/
     public static final String RESOURCE_DISPLAY_NAME_FIELD="(//input[@type='text'])[4]";
-	/**Locator Type = xpath*/
+    
+    /** RESOURCE_DESCRIPTION_AREA locator to Resource Description text area*/
     public static final String RESOURCE_DESCRIPTION_AREA="//textarea";
-    /**Locator Type = cssSelector*/
+    
+    /** SAVE_BUTTON locator to Save button*/
     public static final String SAVE_BUTTON="button.info";
 }
+	   
