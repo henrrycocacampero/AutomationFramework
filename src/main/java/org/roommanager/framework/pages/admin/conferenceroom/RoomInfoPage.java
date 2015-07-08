12f5919fa21d0e1b1f3a32edc58f5f698684d983
unknown <Administrator@CUITPO2001.fundacion-jala.local>
2015-07-08 19:48:41 -0400

    Test case copleted Verify if a room name is disabled

--- src/main/java/org/roommanager/framework/pages/admin/conferenceroom/RoomInfoPage.java
+++ src/main/java/org/roommanager/framework/pages/admin/conferenceroom/RoomInfoPage.java
@@ -4,26 +4,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.roommanager.framework.models.admin.conferencerooms.RoomInfoPageConstant;
 
 public class RoomInfoPage extends ConferenceRoomTopMenu{
 
 	@FindBy(xpath = RoomInfoPageConstant.SAVE_BUTTON_ROOM)
-	private WebElement saveButton_Room;
+	private WebElement saveButtonRoom;
 	@FindBy(css = RoomInfoPageConstant.SAVE_CANCEL_ROOM)
-	private WebElement saveCancel_Room;
+	private WebElement saveCancelRoom;
 	@FindBy(xpath = RoomInfoPageConstant.POWER_BUTTON_ROOM)
 	private WebElement powerButton_Room;
 	@FindBy(xpath = RoomInfoPageConstant.DISPLAYNAME_ROOM_TEXT_FIELD)
-	private WebElement displaynameRoomText_Field;
+	private WebElement displaynameRoomTextField;
 	@FindBy(xpath = RoomInfoPageConstant.CODE_ROOM_TEXT_FIELD)
-	private WebElement codeRoomText_Field;
+	private WebElement codeRoomTextField;
 	@FindBy(xpath = RoomInfoPageConstant.CAPACITY_ROOM_TEXT_FIELD)
-	private WebElement capacityRoomText_Field;
+	private WebElement capacityRoomTextField;
 	@FindBy(css = RoomInfoPageConstant.LOCATION_BUTTON)
-	private WebElement location_Button;
+	private WebElement locationButton;
 	@FindBy(xpath = RoomInfoPageConstant.LOCATION_LIST)
-	private WebElement Location_List;
+	private WebElement LocationList;
+	@FindBy(xpath = RoomInfoPageConstant.ROOM_NAME_TEXT_FIELD)
+	private WebElement roomNameTextField;
 	private WebDriver driver;
 	
 	public RoomInfoPage(WebDriver driver) {
@@ -31,9 +35,63 @@ public class RoomInfoPage extends ConferenceRoomTopMenu{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	public boolean RoomNameIsDisable(){
+		new WebDriverWait(driver,60)
+			.until(ExpectedConditions.visibilityOf(roomNameTextField));
+		boolean isRoomNameTextFieldDisabled = 
+				roomNameTextField.isEnabled() == true? false:true;
+		return isRoomNameTextFieldDisabled;
+	}
+	public ConferenceRoomPage clickButtonSaveUpdateInfoRoom(){
+		(new WebDriverWait(driver,60)).until(ExpectedConditions.visibilityOf(saveButtonRoom));
+		saveButtonRoom.click();
+		return new ConferenceRoomPage(driver); 
+	}
+
+	public ConferenceRoomPage clickButtonCancelUpdateInfoRoom(){
+		(new WebDriverWait(driver,60)).until(ExpectedConditions.visibilityOf(saveCancelRoom));
+		saveCancelRoom.click();
+		return new ConferenceRoomPage(driver); 
+	}
+
+	public String getDisplayNameRoom(){
+		WebElement textFieldDisplayNameRoom = new WebDriverWait(driver,60).
+				until(ExpectedConditions.visibilityOf(displaynameRoomTextField));
+		String getNameRoom = textFieldDisplayNameRoom.getAttribute("Value");
+		return getNameRoom ;
+	}
 	
-
-
+	public void setDisplayNameRoom(String DisplayNameRoom){
+		new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOf(displaynameRoomTextField));	
+		displaynameRoomTextField.clear();
+		displaynameRoomTextField.sendKeys(DisplayNameRoom);
+	}
+	
+	public String getCodeRoom(){
+		WebElement textFieldCodeRoom = new WebDriverWait(driver,60).
+				until(ExpectedConditions.visibilityOf(codeRoomTextField));
+		String getCodeRoom = textFieldCodeRoom.getAttribute("Value");
+		return getCodeRoom ;
+	}
+	
+	public void setCodeRoom(String CodeRoom){
+		new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOf(codeRoomTextField));	
+		codeRoomTextField.clear();
+		codeRoomTextField.sendKeys(CodeRoom);
+	}
+	
+	public String getCapacityRoom(){
+		WebElement textFieldCapacityRoom = new WebDriverWait(driver,60).
+				until(ExpectedConditions.visibilityOf(capacityRoomTextField));			
+		String getCapacityRoom = textFieldCapacityRoom.getAttribute("Value");
+		return getCapacityRoom ;
+	}
+	
+	public void setCapacityRoom(String CapacityRoom){
+		new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOf(capacityRoomTextField));
+		capacityRoomTextField.clear();
+		capacityRoomTextField.sendKeys(CapacityRoom);
+	}	
 }
 
 
