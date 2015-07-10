6c426ed337e147f4ce78565600cfb3180b058842
Daneiva <daneiva@gmail.com>
2015-07-10 16:54:09 -0400

    Update - OutOfOrderTestCases - fix and Add methods

--- src/main/java/org/roommanager/framework/pages/admin/conferenceroom/OutOfOrderPage.java
+++ src/main/java/org/roommanager/framework/pages/admin/conferenceroom/OutOfOrderPage.java
@@ -2,10 +2,7 @@ package org.roommanager.framework.pages.admin.conferenceroom;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
-import java.util.Random;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,7 +12,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.roommanager.framework.models.admin.conferencerooms.ConferenceRoomConstant;
 import org.roommanager.framework.models.admin.conferencerooms.OutOfOrderConstant;
 import org.roommanager.framework.utilities.common.LogManager;
-
 
 public class OutOfOrderPage {
 
@@ -39,111 +35,161 @@ public class OutOfOrderPage {
 	private WebElement descriptionOutOfOrder;
 	@FindBy (xpath = OutOfOrderConstant.SAVE_BUTTON_OUT_OF_ORDER) 
 	private WebElement saveButtonOutOfOrder;
-	
 	@FindBy (css = ConferenceRoomConstant.TITLE_TABLE_ROOMS) 
 	private WebElement titleTableRooms;
-		
-
+	@FindBy (xpath= OutOfOrderConstant.ERROR_MESSAGE_WITHOUT_TITLE)
+	private WebElement errorMessageWhitOutTitle;	
+	@FindBy (xpath= OutOfOrderConstant.TOP_ARROW_START_HOUR)
+	private WebElement topArrowStartHour;
+	@FindBy (xpath= OutOfOrderConstant.TOP_LOWER_START_HOUR)
+	private WebElement lowerArrowStartHour;
+	@FindBy (xpath = OutOfOrderConstant.ERROR_MESSAGE_TO_GREATER_FROM)
+	private WebElement onOfScheduelButton;
+	@FindBy (xpath = OutOfOrderConstant.ON_OFF_SCHEDULE_BUTTON)
+	private WebElement errorMessageToGreaterFrom;
+	@FindBy (xpath = OutOfOrderConstant.SEND_MAIL_CHECKBOX)
+	private WebElement sendMailCheckBox;
+	
 	public OutOfOrderPage(WebDriver driver){
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 	
-	public String getTitle(){
-		return getValueTextField(new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(titleOutOfOrder)));
-	}
-	
-	public void setStartTime(){
-		ArrayList<String> startTime= intervalTime();
-		System.out.println(startTime);
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(startHourOutOfOrder));
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(startMinsOutOfOrder));
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(startAmPmOutOfOrder));
-		startHourOutOfOrder.clear();
-		startHourOutOfOrder.sendKeys(startTime.get(0));
-		startMinsOutOfOrder.clear();
-		startMinsOutOfOrder.sendKeys(startTime.get(1));
-		startAmPmOutOfOrder.clear();
-		startAmPmOutOfOrder.sendKeys(startTime.get(2));
-		    //Logger.info("Set <To: Time Out Of Order> by: "+ startTime.get(0)+":"+ startTime.get(1) +" "+ startTime.get(2));
-	}
-	public void setEndTime(){
-		ArrayList<String> endTime= intervalTime();
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(endHourOutOfOrder));
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(endMinsOutOfOrder));
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(endAmPmOutOfOrder));
-		endHourOutOfOrder.clear();
-		startHourOutOfOrder.sendKeys(endTime.get(3));
-		endMinsOutOfOrder.clear();
-		startHourOutOfOrder.sendKeys(endTime.get(4));
-		endAmPmOutOfOrder.clear();
-		startHourOutOfOrder.sendKeys(endTime.get(5));		
-		    //LoggerManager.info("Set <To: Time Out Of Order> by: "
-		    					//+ endTime.get(3) +":"+ endTime.get(4)+" "+ endTime.get(5));
-	}
-	public void setDescription(){
-		
-		new WebDriverWait(driver,60).
-		until(ExpectedConditions.visibilityOf(descriptionOutOfOrder));
-		descriptionOutOfOrder.clear();
-		descriptionOutOfOrder.sendKeys("OutOfOrder in the room");
-		LogManager.info("OutOfOrderPage - Description value in the room ");	
-			
-	}
-	public String setRandomTitle() {
-	
-	    String [] combo = { "Temporarily Out of Order", 
-			        		"Closed for maintenance", 
-			        		"Closed for reparations", 
-			        		"Reserved for a special event",
-	    					"Different: AGARSTSASDFAS"};        
-	    Random random = new Random();
-	    // randomly selects an index from the combo
-	    int select = random.nextInt(combo.length); 
-	    new WebDriverWait(driver,80).
-	    until(ExpectedConditions.visibilityOf(titleOutOfOrder));        
-	            
-	    titleOutOfOrder.clear();    
-		titleOutOfOrder.sendKeys(combo[select]);
-		//LoggerManager.info("Random Title: "+combo[select]);
-	return combo[select];
-	}	
-	public static String getValueTextField(WebElement textFieldName){
-		
-		String getNameRoom = textFieldName.getAttribute("Value");
-		LogManager.info("The value is: <" + getNameRoom+ ">");
-		return getNameRoom;
-	}	
-	public static ArrayList<String> intervalTime() {
-		  ArrayList<String> intervalTime = new ArrayList<String>(); 		 
-		  Calendar time = Calendar.getInstance();  
-		  
-		    intervalTime.add((time.get(Calendar.HOUR)+1)+"");
-			intervalTime.add(time.get(Calendar.MINUTE)+"");
-			if ((time.get(Calendar.AM_PM)+"").equals("0")){				
-				intervalTime.add("AM");}
-			else{
-				intervalTime.add("PM");}
-			intervalTime.add((time.get(Calendar.HOUR)+2)+"");
-			intervalTime.add(time.get(Calendar.MINUTE)+"");
-			if ((time.get(Calendar.AM_PM)+"").equals("0")){				
-				intervalTime.add("AM");}
-			else{
-				intervalTime.add("PM");}	
-			
-			return intervalTime;
-	}
+	/**
+	 * clickSaveButtonOutOfOrder: It click on Save Button.
+	 * @return OutOfOrderPage
+	 */
 	public OutOfOrderPage clickSaveButtonOutOfOrder(){
 		(new WebDriverWait(driver,60)).until(ExpectedConditions.visibilityOf(saveButtonOutOfOrder));
 		saveButtonOutOfOrder.click();
 		LogManager.info("Was Clicking Save Button Out Of Order");
-		return new OutOfOrderPage(driver);
+		return this;
+	}
+	
+	/**
+	 * setDescription: It enters Out Of Order Description 
+	 */
+	public OutOfOrderPage setDescription(String setDescription){
+		
+		new WebDriverWait(driver,60).until(ExpectedConditions
+								.visibilityOf(descriptionOutOfOrder));
+		descriptionOutOfOrder.clear();
+		descriptionOutOfOrder.sendKeys(setDescription);
+		LogManager.info("OutOfOrderPage - Description value in the room ");	
+		return this;
+	}
+	
+	/**
+	 * setDescription: It enters empty title in an Out Of Order. 
+	 * @return String
+	 */
+	public OutOfOrderPage setTitle(String nameTitle){
+		
+		new WebDriverWait(driver,80).until(ExpectedConditions
+				 					 .visibilityOf(titleOutOfOrder));        		            
+		titleOutOfOrder.clear();    
+		titleOutOfOrder.sendKeys(nameTitle);
+		LogManager.info("It enters empty title in an Out Of Order");
+		return this;
+	}
+	
+	/**
+	 * existOutOfOrder: Making Verification if Error Message is showed when 
+	 * created an OutOfORder without Title
+	 * @return boolean
+	 */
+	public boolean existErrorMessageWhithoutTitle(){
+		boolean errorMessageExist=false;
+		new WebDriverWait(driver,80).until(ExpectedConditions
+									.visibilityOf(descriptionOutOfOrder));
+			if(errorMessageWhitOutTitle.isDisplayed()){
+				errorMessageExist=true;
+			}	
+		LogManager.info("Title Subject: <" + getTitleOutOfOrder()+ ">");
+		return errorMessageExist;
+	}
+	
+	/**
+	 * getTitleOutOfOrder: It retrieves the specified title of a OutOfOrder.
+	 * @return String
+	 */
+	public String getTitleOutOfOrder(){
+		new WebDriverWait(driver,60).until(ExpectedConditions
+									.visibilityOf(titleOutOfOrder));
+		String getTitle = titleOutOfOrder.getAttribute("value");
+		return getTitle ;
+	}
+	
+	/**
+	 * intervalTime: Returns a start<Hour> and end<Hour> with an hour intervalod
+	 * @return array
+	 */
+	public static ArrayList<String> intervalTime() {
+		ArrayList<String> intervalTime = new ArrayList<String>(); 		 
+		Calendar time = Calendar.getInstance();  
+		  
+		intervalTime.add((time.get(Calendar.HOUR)+1)+"");
+		intervalTime.add(time.get(Calendar.MINUTE)+"");
+		if ((time.get(Calendar.AM_PM)+"").equals("0")){		
+			intervalTime.add("AM");
+		}
+		else{
+			intervalTime.add("PM");}
+			intervalTime.add((time.get(Calendar.HOUR)+2)+"");
+			intervalTime.add(time.get(Calendar.MINUTE)+"");
+			if ((time.get(Calendar.AM_PM)+"").equals("0")){				
+				intervalTime.add("AM");
+			}
+			else{
+				intervalTime.add("PM");
+			}			
+		return intervalTime;
+	}
+	
+	/**
+	 *clickTopArrowStartHour: It click on Low Arrow Start of Start Hour Button Button */
+	public OutOfOrderPage clickTopArrowStartHour(){
+		(new WebDriverWait(driver,60)).until(ExpectedConditions
+									  .visibilityOf(lowerArrowStartHour));
+		lowerArrowStartHour.click();
+		LogManager.info("Was Clicking Low Arrow Start of Start Hour Button");
+		return this;
+	}
+	
+	/**
+	 * clickLowerArrowStartHour: It click on Top Arrow Start of Start Hour Button Button*/
+	public OutOfOrderPage clickLowerArrowStartHour(){
+		(new WebDriverWait(driver,60)).until(ExpectedConditions
+									  .visibilityOf(topArrowStartHour));
+		topArrowStartHour.click();
+		LogManager.info("Was Clicking Top Arrow Start of Start Hour Button");
+		return this;
+	}
+	
+	/** existOutOfOrder: Making Verification if Error Message is showed when 
+	 * created an OutOfORder and "To" field must be greater than "From" field.
+	 * @return boolean
+	 */
+	public boolean errorMessageToGreaterFrom(){
+		boolean errorMessageExist=false;
+		new WebDriverWait(driver,80).until(ExpectedConditions
+									.visibilityOf(errorMessageToGreaterFrom));
+		if(errorMessageToGreaterFrom.isDisplayed()){
+			errorMessageExist=true;
+		}	
+		LogManager.info("Error Message: <" + errorMessageToGreaterFrom.getText()+ ">");
+		return errorMessageExist;
 	}	
+	/**
+	 * clickCalendarButton: It clicks on the Calendar Button 
+	 * 
+	 * @return ConferenceRoomPage
+	 */
+	public OutOfOrderPage  clickScheduleButton() {
+		(new WebDriverWait(driver, 60)).until(ExpectedConditions
+				.visibilityOf(onOfScheduelButton));
+		onOfScheduelButton.click();
+		LogManager.info("Close button was clicked");
+		return this;
+	}
 }
