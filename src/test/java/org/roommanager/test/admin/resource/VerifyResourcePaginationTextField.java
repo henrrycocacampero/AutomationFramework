56bf4a35c488461caf13d00fbaf01170e4c32b05
jimmy29011993 <jimmy2901993@gmail.com>
2015-07-10 12:08:19 -0400

    Create - Verify resource association quantity - Create the test, the Resource Associations Page with methods to retrieve the associations

--- src/test/java/org/roommanager/test/admin/resource/VerifyResourcePaginationTextField.java
+++ src/test/java/org/roommanager/test/admin/resource/VerifyResourcePaginationTextField.java
@@ -7,43 +7,44 @@ import org.roommanager.framework.utilities.common.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class VerifyResourcePaginationInvalid extends TestBase{
-	
-	/** invalidPage represents an invalid page*/
-	private String invalidPage = "abc";
+/**
+ * This class contains a resouce's test case
+ * @author Jimmy Maldonado
+ *
+ */
+public class VerifyResourcePaginationTextField extends TestBase{
 	
 	/** 
-	 * expectedPage represents the expected result to the pagination 
-	 * text field.
+	 * expectedPage represents the page number from resource's 
+	 * page
 	 * */
-	private String expectedPage = "";
+	private String expectedPage = "10";
 	
 	/** 
 	 * errorMessage: It contains the error message that would appear 
-	 * if test case fails.
+	 * if test case fails
 	 */
 	private String errorMessage = "The test failed because entered page "
-			+ "was found";
+			+ "was not found";
 	
 	/**
-	 * This method contains the steps for the test case: Verify that 
-	 * the pagination text field in the table of the resource does not 
-	 * accepted alphabetic values.
+	 * This method contains the steps for the test case: Verify 
+	 * that the pagination text field in the table of the resource 
+	 * accept only int numbers.
 	 */
     @Test
-    public void verifyResourcePaginationInvalid() {
-
-		LoginPage login = new LoginPage(driver);
+    public void verifyResourcePaginationTextField() {
+    	
+    	LoginPage login = new LoginPage(driver);
 		
 		HomePage homePage = login.clickSignInButton();
 		
 		ResourcePage resource =  homePage.selectResourcesLink();
 		
-		resource.setPaginationTextField(invalidPage);
+		resource.setPaginationTextField(expectedPage);
 		
 		String actualPage = resource.getPaginationTextField();
 		
-		Assert.assertEquals(expectedPage, actualPage, errorMessage);
-		
+		Assert.assertEquals(actualPage, expectedPage, errorMessage);
     }
 }
