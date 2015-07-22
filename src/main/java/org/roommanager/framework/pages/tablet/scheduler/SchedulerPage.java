b37c283ecd09de486021151abe1b9d7dc6a1c8a5
jimmy29011993 <jimmy2901993@gmail.com>
2015-07-22 11:52:00 -0400

    Update Scheduler page

--- src/main/java/org/roommanager/framework/pages/tablet/scheduler/SchedulerPage.java
+++ src/main/java/org/roommanager/framework/pages/tablet/scheduler/SchedulerPage.java
@@ -51,7 +51,7 @@ public class SchedulerPage extends PageFactory{
 	}
 	public SchedulerPage setOrganizerTextField(String organizer){
 		(new WebDriverWait(driver, 30))
-		.until(ExpectedConditions.visibilityOf(organizerTextField));
+			.until(ExpectedConditions.visibilityOf(organizerTextField));
 		organizerTextField.clear();
 		organizerTextField.sendKeys(organizer);
 		LogManager.info("Organizer: <"+ organizer +"> was entered");
@@ -59,7 +59,7 @@ public class SchedulerPage extends PageFactory{
 	}
 	public SchedulerPage setSubjectTextField(String subject){
 		(new WebDriverWait(driver, 30))
-		.until(ExpectedConditions.visibilityOf(subjectTextField));
+			.until(ExpectedConditions.visibilityOf(subjectTextField));
 		subjectTextField.clear();
 		subjectTextField.sendKeys(subject);
 		LogManager.info("Subject: <"+ subject +"> was entered");
@@ -67,7 +67,7 @@ public class SchedulerPage extends PageFactory{
 	}
 	public SchedulerPage setAttendeesTextField(String attendee){
 		(new WebDriverWait(driver, 30))
-		.until(ExpectedConditions.visibilityOf(attendeesTextField));
+			.until(ExpectedConditions.visibilityOf(attendeesTextField));
 		attendeesTextField.clear();
 		attendeesTextField.sendKeys(attendee + ";");
 		LogManager.info("Attendee: <"+ attendee +"> was entered");
@@ -75,7 +75,7 @@ public class SchedulerPage extends PageFactory{
 	}
 	public SchedulerPage setBodyTextArea(String body){
 		(new WebDriverWait(driver, 30))
-		.until(ExpectedConditions.visibilityOf(bodyTextArea));
+			.until(ExpectedConditions.visibilityOf(bodyTextArea));
 		bodyTextArea.clear();
 		bodyTextArea.sendKeys(body);
 		LogManager.info("Body: <"+ body +"> was entered");
@@ -83,7 +83,7 @@ public class SchedulerPage extends PageFactory{
 	}
 	public CredentialsPage clickCreateButton(){
 		(new WebDriverWait(driver, 20))
-		.until(ExpectedConditions.visibilityOf(createButton));
+			.until(ExpectedConditions.visibilityOf(createButton));
 		createButton.click();
 		LogManager.info("Create Button was clicked");
 		return new CredentialsPage(driver);
@@ -91,7 +91,7 @@ public class SchedulerPage extends PageFactory{
 	public boolean isSubjectFieldErrorMessagePresent(){
 		String expectedErrorMessage = "Subject is required";
 		(new WebDriverWait(driver,60))
-		.until(ExpectedConditions.visibilityOf(meetingSubjectErrorMessage));
+			.until(ExpectedConditions.visibilityOf(meetingSubjectErrorMessage));
 		String errorMessage = meetingSubjectErrorMessage.getText();
 		LogManager.info("Error Message: <"+ errorMessage +"> was displayed");
 		return errorMessage.equals(expectedErrorMessage);
@@ -100,7 +100,7 @@ public class SchedulerPage extends PageFactory{
 	public boolean isOrganizerFieldErrorMessagePresent(){
 		String expectedErrorMessage = "Organizer is required";
 		(new WebDriverWait(driver,60))
-		.until(ExpectedConditions.visibilityOf(organizerSubjectErrorMessage));
+			.until(ExpectedConditions.visibilityOf(organizerSubjectErrorMessage));
 		String errorMessage = organizerSubjectErrorMessage.getText();
 		LogManager.info("Error Message: <"+ errorMessage +"> was displayed");
 		return errorMessage.equals(expectedErrorMessage);
@@ -108,33 +108,33 @@ public class SchedulerPage extends PageFactory{
 	
 	public String getSuccessfulMessage(){
 		(new WebDriverWait(driver,60))
-		.until(ExpectedConditions.visibilityOf(confirmationMessage));
+			.until(ExpectedConditions.visibilityOf(confirmationMessage));
 		return confirmationMessage.getText();
 	}
 	public SchedulerPage clickOnMeetingBox(String subject){
 		getMeetingBoxBySubject(subject).click();
 		(new WebDriverWait(driver,20))
-		.until(ExpectedConditions.visibilityOf(updateButton));
+			.until(ExpectedConditions.visibilityOf(updateButton));
 		LogManager.info("Meeting <" + subject + "> was clicked");
 		return this;
 	}
 	public CredentialsPage clickRemoveButton(){
 		(new WebDriverWait(driver,30))
-		.until(ExpectedConditions.visibilityOf(removeButton));
+			.until(ExpectedConditions.visibilityOf(removeButton));
 		removeButton.click();
 		LogManager.info("Remove Button was clicked");
 		return new CredentialsPage(driver);
 	}
 	public CredentialsPage clickUpdateButton(){
 		(new WebDriverWait(driver,30))
-		.until(ExpectedConditions.visibilityOf(updateButton));
+			.until(ExpectedConditions.visibilityOf(updateButton));
 		updateButton.click();
 		LogManager.info("Update Button was clicked");
 		return new CredentialsPage(driver);
 	}
 	private WebElement getAttendee(String attendee){
 		(new WebDriverWait(driver,60))
-		.until(ExpectedConditions.visibilityOf(attendeesList));
+			.until(ExpectedConditions.visibilityOf(attendeesList));
 		WebElement list = driver.findElement(By.xpath(SchedulerConstant.ATTENDEES_LIST));
 		List<WebElement> attendees = list.findElements(By.xpath(SchedulerConstant.ATTENDEE_TEXT));
 		for(WebElement element : attendees){
