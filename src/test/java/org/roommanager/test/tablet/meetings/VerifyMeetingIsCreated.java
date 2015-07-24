c702664b06e33381a9b8f2073c5381d4c0122459
jimmy29011993 <jimmy2901993@gmail.com>
2015-07-24 11:46:17 -0400

    Update - Verify that is possible to create a meeting - Update test case acording to the new version of Room Manager

--- src/test/java/org/roommanager/test/tablet/meetings/VerifyMeetingIsCreated.java
+++ src/test/java/org/roommanager/test/tablet/meetings/VerifyMeetingIsCreated.java
@@ -1,37 +1,70 @@
 package org.roommanager.test.tablet.meetings;
 
-import org.roommanager.framework.pages.tablet.home.HomePage;
 import org.roommanager.framework.pages.tablet.scheduler.CredentialsPage;
 import org.roommanager.framework.pages.tablet.scheduler.SchedulerPage;
-import org.roommanager.framework.pages.tablet.settings.SettingsPage;
-import org.roommanager.framework.utilities.api.admin.EmailServerApi;
+import org.roommanager.framework.pages.tablet.settings.ConnectionPage;
+import org.roommanager.framework.pages.tablet.settings.NavigationPage;
 import org.roommanager.framework.utilities.api.tablet.MeetingApi;
 import org.roommanager.framework.utilities.common.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.roommanager.framework.utilities.common.PropertiesReader;
 
+/**
+ * This class contains a test case to verify that is possible
+ * to create a meeting
+ * @author Qadev02
+ *
+ */
 public class VerifyMeetingIsCreated extends TestBase{
-	private String username = PropertiesReader.getUsername();
-	private String password = PropertiesReader.getPassword();
-	private String roomName = "Room07";
-	private String meetingSubject = "Subject Meeting Pablo";
-	private String errorMessage = "The Test failed because the created couldn't be found in the Scheduler Page";
 	
+	/** username: It represents the meeting's organizer*/
+	private String username = PropertiesReader.getUsername();
+	
+	/** password: It contains the user's password*/
+	private String password = PropertiesReader.getPassword();
+	
+	/** 
+	 * roomName: It represents the conference room in which the 
+	 * meeting will be created
+	 */
+	private String roomName = "Room10";
+	
+	/** 
+	 * meetingSubject: It represents the subject of the meeting to 
+	 * be created
+	 */
+	private String meetingSubject = "Subject";
+	
+	/** 
+	 * errorMessage: It represents the Error Message 
+	 * that will be displayed if the test fails
+	 */
+	private String errorMessage = "The Test failed because the meeting"
+			+ " created couldn't be found in the Scheduler Page";
+	
+	/**
+	 * This method contains the test case's steps and assertions
+	 */
 	@Test 
-	public void VerifyAMeetingIsCreated(){
-		SettingsPage settings = new SettingsPage(driver);
+	public void verifyAMeetingIsCreated(){
 		
-		HomePage home = settings
-			.waitForSettingPageToLoad()
-			.clickRoomItem(roomName)
-			.clickAcceptButton();
+		ConnectionPage connection = new ConnectionPage(driver);
 		
-		SchedulerPage scheduler = home
-			.clickSchedulerLink();
-			
+		connection
+			.enterServiceUrl(PropertiesReader.getRoomManagerApi())
+			.clickSaveButton();
+		
+		NavigationPage navigation = connection
+			.clickNavigationLink()
+			.clickDefaultRoomComboBox()
+			.selectConferenceRoomByName(roomName)
+			.clickSaveButton();
+		
+		SchedulerPage scheduler = navigation
+			.clickOnSchedulerPageLink();
+					
 		CredentialsPage credential = scheduler
 			.setOrganizerTextField(username)
 			.setSubjectTextField(meetingSubject)
@@ -47,14 +80,11 @@ public class VerifyMeetingIsCreated extends TestBase{
 		Assert.assertTrue(meetingExists, errorMessage);
 	}
 	
+	/**
+	 * This method deletes the meeting created by the test case
+	 */
 	@AfterTest
 	public void testTearDown(){
 		MeetingApi.deleteMeetingBySubjectName(roomName, meetingSubject);
-	}
-	
-	@BeforeTest
-	public void beforeTest(){
-		if(EmailServerApi.getEmailServiceId() == null)
-			EmailServerApi.createEmailServer(PropertiesReader.getUsername(), PropertiesReader.getPassword(), PropertiesReader.getExchangeHostName());
 	}
 }
