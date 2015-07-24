32f53761544d07b807402aaaf13659e0253404ad
jimmy29011993 <jimmy2901993@gmail.com>
2015-07-24 15:07:43 -0400

    Fix - Verify tha a meeting is not created with invalid organizer and subject

--- src/test/java/org/roommanager/test/tablet/meetings/VerifyMeetingInvalidSubject.java
+++ src/test/java/org/roommanager/test/tablet/meetings/VerifyMeetingInvalidSubject.java
@@ -1,8 +1,8 @@
 package org.roommanager.test.tablet.meetings;
 
-import org.roommanager.framework.pages.tablet.home.HomePage;
 import org.roommanager.framework.pages.tablet.scheduler.SchedulerPage;
-import org.roommanager.framework.pages.tablet.settings.SettingsPage;
+import org.roommanager.framework.pages.tablet.settings.ConnectionPage;
+import org.roommanager.framework.pages.tablet.settings.NavigationPage;
 import org.roommanager.framework.utilities.api.admin.EmailServerApi;
 import org.roommanager.framework.utilities.common.PropertiesReader;
 import org.roommanager.framework.utilities.common.TestBase;
@@ -18,14 +18,19 @@ public class VerifyMeetingInvalidSubject extends TestBase{
 	
 	@Test 
 	public void VerifyMeetingInvalidSubjectErrorMessage(){
-		SettingsPage settings = new SettingsPage(driver);
+		ConnectionPage connection = new ConnectionPage(driver);
 		
-		HomePage home = settings
-			.waitForSettingPageToLoad()
-			.clickRoomItem(roomName)
-			.clickAcceptButton();
+		connection
+			.enterServiceUrl(PropertiesReader.getRoomManagerApi())
+			.clickSaveButton();
 		
-		SchedulerPage scheduler = home
+		NavigationPage navigation = connection
+			.clickNavigationLink()
+			.clickDefaultRoomComboBox()
+			.selectConferenceRoomByName(roomName)
+			.clickSaveButton();
+		
+		SchedulerPage scheduler = navigation
 			.clickOnSchedulerPageLink();
 			
 		scheduler
